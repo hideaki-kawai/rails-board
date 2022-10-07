829557c3acf6425cb3857f1e05cd21e35b48fe1f
hideaki-kawai <h.kawai.tech@gmail.com>
2022-10-08 00:16:56 +0900

    コメント一覧表示実装

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -26,7 +26,7 @@ class BoardsController < ApplicationController
 
     def show
         # @board = Board.find(params[:id])
-        @comment = @board.comments.new
+        @comment = Comment.new(board_id: @board.id)
     end
 
     def edit

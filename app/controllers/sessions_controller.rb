945bf0bf81ad660f3dd23ef3a9dc4f3003500b8c
hideaki-kawai <h.kawai.tech@gmail.com>
2022-10-10 00:54:58 +0900

    ログイン、ログアウト機能実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,19 @@
 class SessionsController < ApplicationController
+  # ログイン処理
   def create
+    # Userモデルから該当するユーザーを取得
+    user = User.find_by(name: params[:session][:name])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to mypage_path
+    else
+      render 'home/index'
+    end
   end
 
+  # ログアウト処理
   def destroy
+    session.delete(:user_id)
+    redirect_to root_path
   end
 end
